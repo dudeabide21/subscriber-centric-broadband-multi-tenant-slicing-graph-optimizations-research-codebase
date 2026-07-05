1ced1a33c6a682d2f4d44155a3666d87535a0617
Dipesh <dipeshbhatta2061@gmail.com>
2026-07-05 15:13:24 +0545

    chore(queueing): export queueing helpers
    Re-exports the queueing and LEO helper functions from the package root.

---
+++ src/scb/queueing/__init__.py
@@ -0,0 +1,23 @@
+"""Queueing, diffusion, and effective-bandwidth helpers."""
+
+from scb.leo import leo_backoff_delay
+from scb.queueing.diffusion_estimator import (
+    diffusion_coefficient,
+    mean_arrival_rate,
+)
+from scb.queueing.effective_bandwidth import effective_bandwidth
+from scb.queueing.effective_service import (
+    effective_service_capacity,
+    effective_service_capacity_from_sequence,
+)
+from scb.queueing.overflow_risk import overflow_risk_indicator
+
+__all__ = [
+    "diffusion_coefficient",
+    "effective_bandwidth",
+    "effective_service_capacity",
+    "effective_service_capacity_from_sequence",
+    "leo_backoff_delay",
+    "mean_arrival_rate",
+    "overflow_risk_indicator",
+]
