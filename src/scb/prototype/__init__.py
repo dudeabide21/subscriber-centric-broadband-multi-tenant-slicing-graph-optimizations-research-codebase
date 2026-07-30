1442b0a3a24750f8869ec6660bfac1b77f2354a1
Dipesh Bhatta <dipeshbhatta2061@gmail.com>
2026-07-30 05:53:38 +0000

    added render_prototype summary to aggregate

--- src/scb/prototype/__init__.py
+++ src/scb/prototype/__init__.py
@@ -8,6 +8,7 @@ from scb.prototype.evidence import (
     load_prototype_run,
     validate_required_fields,
 )
+from scb.prototype.summary import render_prototype_summary
 
 __all__ = [
     "COMMON_REQUIRED_FIELDS",
@@ -15,5 +16,6 @@ __all__ = [
     "classify_evidence_record",
     "load_json_record",
     "load_prototype_run",
+    "render_prototype_summary",
     "validate_required_fields",
 ]
